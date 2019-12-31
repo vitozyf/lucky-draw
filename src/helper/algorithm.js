6c92b0e36bf8d866c962adf12911140d8a323116
zhangyongfeng <535069215@qq.com>
2019-12-31 18:44:02 +0800

    chrome v8 MathRandom

--- src/helper/algorithm.js
+++ src/helper/algorithm.js
@@ -1,4 +1,21 @@
 /**
+ * chrome v8 实现
+ */
+/*
+// ECMA 262 - 15.8.2.14
+	var rngstate;  // Initialized to a Uint32Array during genesis.
+	function MathRandom() {
+		var r0 = (MathImul(18030, rngstate[0] & 0xFFFF) + (rngstate[0] >>> 16)) | 0;
+		rngstate[0] = r0;
+		var r1 = (MathImul(36969, rngstate[1] & 0xFFFF) + (rngstate[1] >>> 16)) | 0;
+		rngstate[1] = r1;
+		var x = ((r0 << 16) + (r1 & 0xFFFF)) | 0;
+		// Division by 0x100000000 through multiplication by reciprocal.
+		return (x < 0 ? (x + 0x100000000) : x) * 2.3283064365386962890625e-10;
+	} 
+*/
+
+/**
  * 取范围内随机整数
  * @param {number} minNum
  * @param {number} maxNum
