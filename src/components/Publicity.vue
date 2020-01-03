cd7de73b40b7f517ed95d3319041221209d817f9
vito <vitozhangyf@outlook.com>
2020-01-03 21:14:27 +0800

    publicity

--- src/components/Publicity.vue
+++ src/components/Publicity.vue
@@ -57,7 +57,7 @@ export default {
 .c-Publicity {
   height: 100%;
   // width: 1000px;
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(255, 255, 255, 0.1);
   margin: 0 auto;
   position: relative;
   overflow: hidden;
